7579f9385a6e867b45bfdc672aa0e286d56237d2
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-05-15 01:23:52 -0600

    update

--- code/js/localstorage.js
+++ code/js/localstorage.js
@@ -31,22 +31,17 @@ function clickCount() {
 
 function createPerson() {
 	var firstName = document.getElementById("firstName").value;
-	var lastName = document.getElementById("lastName").value;
-	var birthday = document.getElementById("birthday").value;
-	var email = document.getElementById("email").value;
-	var phone = document.getElementById("phone").value;
+	var fruit = document.getElementById("fruit").value;
+	var color = document.getElementById("color").value;
 
-	var person = {firstName: firstName, lastName: lastName, birthday: birthday, email: email, phone: phone};
+
+	var person = {firstName: firstName, fruit: fruit, color: color};
 
 	localStorage.setItem('user', JSON.stringify(person));
 	localStorage.setItem('browser-name', navigator.appCodeName);
-	localStorage.setItem('browser-version', navigator.appVersion);
-	localStorage.setItem('browser-cookies', navigator.cookieEnabled);
 
 	sessionStorage.setItem('user', JSON.stringify(person));
 	sessionStorage.setItem('browser-name', navigator.appCodeName);
-	sessionStorage.setItem('browser-version', navigator.appVersion);
-	sessionStorage.setItem('browser-cookies', navigator.cookieEnabled);
 
 	document.getElementById("userOutputLocal").innerHTML = "Submission successful.";
 }
@@ -54,19 +49,13 @@ function createPerson() {
 function showPerson() {
 	var user = JSON.parse(localStorage.getItem('user'));
 	var browserName = localStorage.getItem('browser-name');
-	var browserVersion = localStorage.getItem('browser-version');
-	var browserCookies = localStorage.getItem('browser-cookies');
 
 	var user2 = JSON.parse(sessionStorage.getItem('user'));
 	var browserName2 = sessionStorage.getItem('browser-name');
-	var browserVersion2 = sessionStorage.getItem('browser-version');
-	var browserCookies2 = sessionStorage.getItem('browser-cookies');
 
-	document.getElementById("userOutputLocal").innerHTML = "Local Storage Output:" + "<br><b>First Name:</b> " + user.firstName + "<br><b>Last Name:</b> " + user.lastName 
-		+ "<br><b>Birthday:</b>" + user.birthday + "<br><b>Email:</b> " + user.email + "<br><b>Phone:</b> " + user.phone + "<br><b>Browser Name:</b> " 
-		+ browserName + "<br><b>Browser Version:</b> " + browserVersion + "<br><b>Browser Cookies Enabled:</b> " + browserCookies;
+	document.getElementById("userOutputLocal").innerHTML = "Local Storage Output:" + "<br><b>First Name:</b> " + user.firstName + "<br><b>Favorite Fruit:</b> " + user.fruit 
+		+ "<br><b>Favorite Color:</b>" + user.color + "<br><b>Browser Name:</b> " + browserName;
 
-	document.getElementById("userOutputSession").innerHTML = "<br>Session Storage Output:" + "<br><b>First Name:</b> " + user2.firstName + "<br><b>Last Name:</b> " + user2.lastName 
-		+ "<br><b>Birthday:</b>" + user2.birthday + "<br><b>Email:</b> " + user2.email + "<br><b>Phone:</b> " + user2.phone + "<br><b>Browser Name:</b> " 
-		+ browserName2 + "<br><b>Browser Version:</b> " + browserVersion2 + "<br><b>Browser Cookies Enabled:</b> " + browserCookies2;
+	document.getElementById("userOutputSession").innerHTML = "<br>Session Storage Output:" + "<br><b>First Name:</b> " + user2.firstName + "<br><b>Favorite Fruit:</b> " + user2.fruit
+		+ "<br><b>Favorite Color:</b>" + user2.color + "<br><b>Browser Name:</b> " + browserName2;
 }
