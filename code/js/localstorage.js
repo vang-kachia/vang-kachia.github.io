63fcfdbf3571cd399d75de20322562fe1ce4d121
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-05-20 21:34:24 -0600

    count

--- code/js/localstorage.js
+++ code/js/localstorage.js
@@ -4,29 +4,22 @@ function Reset() {
 }
 
 function Count() {
-	if (typeof(Storage) !== "undefined") {
-		if (localStorage.clickcount) {
+
 			localStorage.clickcount = Number(localStorage.clickcount) + 1;
-		} else {
-			localStorage.clickcount = 1;
-		}
+		
+		
+		
 
 		document.getElementById("localClicks").innerHTML = "Your Local Storage Count: " + localStorage.clickcount + " click(s)";
-	} else {
-		document.getElementById("localClicks").innerHTML = "Error: Your browser does not support local storage.";
-	}
+	
+	
 
-	if (typeof(Storage) !== "undefined") {
-		if (sessionStorage.clicks) {
+	
 			sessionStorage.clicks = Number(sessionStorage.clicks) + 1;
-		} else {
-			sessionStorage.clicks = 1;
-		}
+		
 
 		document.getElementById("sessionClicks").innerHTML = "Your Session Storage Count: " + sessionStorage.clicks + " click(s)";
-	} else {
-		document.getElementById("sessionClicks").innerHTML = "Error: Your browser does not support session storage.";
-	}
+	
 }
 
 function User() {
