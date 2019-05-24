461164be502073777a2a96727546796014c3a412
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-05-23 20:04:32 -0600

    implement css

--- code/js/loops.js
+++ code/js/loops.js
@@ -1,33 +1,33 @@
 
 function numberLoop() {
     var userInput = document.getElementById("userInput").value;
-    var response;
+    var userResponse;
 
 
     if (userInput < 2) {
-    response = "Try again.";
+        userResponse = "Try again.";
     } else if (userInput < 3) {
-        response = "Sorry, try again!"; 
+        userResponse = "Sorry, try again!"; 
     } else if (userInput < 4) {
-        response = "Guess another number."; 
+        userResponse = "Guess another number."; 
     } else if (userInput < 5) {
-        response = "Not quite.";
+        userResponse = "Not quite.";
     } else if (userInput < 6) {
-        response = "Oops, try again.";
+        userResponse = "Oops, try again.";
     } else if (userInput < 7) {
-        response = "Keep on guessing.";
+        userResponse = "Keep on guessing.";
     } else if (userInput < 8) {
-         response = "Nope. Try again.";
+        userResponse = "Nope. Try again.";
     } else if (userInput < 9) {
-        response = "YOU ARE CORRECT. THAT IS THE RIGHT NUMBER!";
+        userResponse = "YOU ARE CORRECT. THAT IS THE RIGHT NUMBER!";
     } else if (userInput < 10) {
-        response = "That number is too high.";
+        userResponse = "That number is too high.";
     } else if (userInput < 11) {
-        response = "Definitely, try again!";                                                                              
+        userResponse = "Definitely, try again!";                                                                              
     } else {
-         response = "Make sure the number is between 1-10.";
+        userResponse = "That number is out of range.";
     }
-    document.getElementById('numberOutput').innerHTML = response;
+    document.getElementById('numberOutput').innerHTML = userResponse;
         
 
         
