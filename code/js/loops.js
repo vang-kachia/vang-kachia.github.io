bb9499665c39a401fc4547559ae231c68d4a76b1
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-05-23 19:56:02 -0600

    loops

--- code/js/loops.js
+++ code/js/loops.js
@@ -4,28 +4,28 @@ function numberLoop() {
     var response;
 
 
-    if (userInput = 1) {
+    if (userInput < 2) {
     response = "Try again.";
-    } else if (userInput = 2) {
+    } else if (userInput < 3) {
         response = "Sorry, try again!"; 
-    } else if (userInput = 3) {
+    } else if (userInput < 4) {
         response = "Guess another number."; 
-    } else if (userInput = 4) {
+    } else if (userInput < 5) {
         response = "Not quite.";
-    } else if (userInput = 5) {
+    } else if (userInput < 6) {
         response = "Oops, try again.";
-    } else if (userInput = 6) {
+    } else if (userInput < 7) {
         response = "Keep on guessing.";
-    } else if (userInput = 7) {
+    } else if (userInput < 8) {
          response = "Nope. Try again.";
-    } else if (userInput = 8) {
+    } else if (userInput < 9) {
         response = "YOU ARE CORRECT. THAT IS THE RIGHT NUMBER!";
-    } else if (userInput = 9) {
+    } else if (userInput < 10) {
         response = "That number is too high.";
-    } else if (userInput = 10) {
-        response = "Definitely, try again!";                                                                               
+    } else if (userInput < 11) {
+        response = "Definitely, try again!";                                                                              
     } else {
-         response = "That number is out of range.";
+         response = "Make sure the number is between 1-10.";
     }
     document.getElementById('numberOutput').innerHTML = response;
         
