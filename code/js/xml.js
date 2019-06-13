6724c3682876981c4b5d05fb0f7efe1aaedaa856
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-06-13 00:00:28 -0600

    xml

---
+++ code/js/xml.js
@@ -0,0 +1,33 @@
+function countryInfo() {
+
+var countryinfo = new XMLHttpRequest();
+
+countryinfo.open("GET", "jsonfiles/country.json");
+countryinfo.send();
+
+countryinfo.onreadystatechange = function getInfo() {
+  if (countryinfo.readyState === 4) {
+    if (countryinfo.status === 200) {
+	  ccountry = JSON.parse(countryinfo.responseText)
+    } 
+  }
+};
+
+country = document.getElementById("country").value;
+document.getElementById("capital").innerHTML = "<br>";
+document.getElementById("continent").innerHTML = "<br>";
+document.getElementById("longitude").innerHTML = "<br>";
+document.getElementById("latitude").innerHTML = "<br>";
+document.getElementById("code").innerHTML = "<br>";
+
+for (i = 0; i < ccountry.length; i++) {
+
+	if	(ccountry[i].CountryName == country) {
+	document.getElementById("capital").innerHTML = "⦿ Country's Capital: <b>" + ccountry[i].CapitalName + "</b>";
+	document.getElementById("continent").innerHTML = "⦿ Located on the Continent of: <b>" + ccountry[i].ContinentName + "</b>";
+	document.getElementById("longitude").innerHTML = "⦿ Country's Longitude: <b>" + ccountry[i].CapitalLongitude + "</b>";
+	document.getElementById("latitude").innerHTML = "⦿ Country's Latitude: <b>" + ccountry[i].CapitalLatitude + "</b>";
+	document.getElementById("latitude").innerHTML = "⦿ Country's Code: <b>" + ccountry[i].CountryCode + "</b>";
+} 
+}
+}
