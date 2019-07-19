2f64047377bdb7249a0d76a207eab54dce7b1240
vang-kachia <43485295+vang-kachia@users.noreply.github.com>
2019-07-18 21:49:29 -0600

    final

--- code/js/finalproject.js
+++ code/js/finalproject.js
@@ -14,28 +14,29 @@ document.getElementById("localStorageOutput").innerHTML = output.Name + " is bri
 }
 
 
-function create() {
-var newh3 = document.createElement("h3");
-var newfall = document.createTextNode("I AM.");
-newh3.appendChild(newfall);
-var parent = document.getElementById("parent1");
-var div = document.getElementById("dId1");
-parent.insertBefore(newh3, div);
-}
+function adjustPicture() {
+    var picWidth = document.getElementById("picWidth").value;
+    document.getElementById("hunt").style.width = picWidth + '%'; 
+    }
+function adjustPercentage(percentage) {
+    document.getElementById("picSliderOutput").innerHTML = percentage + '%';
+    }
+function adjustPixels(pixels) {
+    document.getElementById("textSliderOutput").innerHTML = pixels + ' px';
+    }
 
+
+function create() {
+        var newh3 = document.createElement("h3");
+        var newfall = document.createTextNode("I AM.");
+        newh3.appendChild(newfall);
+        var parent = document.getElementById("parent1");
+        var div = document.getElementById("dId1");
+        parent.insertBefore(newh3, div);
+        }
+        
 function addTextNode(text) {
 var addText = document.createTextNode(text),
 pId1 = document.getElementById("pId1");
 pId1.appendChild(addText);
 }
-
-function adjustPicture() {
-    var picWidth = document.getElementById("picWidth").value;
-    document.getElementById("hunt").style.width = picWidth + '%'; 
-    }
-    function adjustPercentage(percentage) {
-    document.getElementById("picSliderOutput").innerHTML = percentage + '%';
-    }
-    function adjustPixels(pixels) {
-    document.getElementById("textSliderOutput").innerHTML = pixels + ' px';
-    }
